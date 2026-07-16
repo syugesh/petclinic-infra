24f414b29c1827f3bfba3fd40b9f1c2d9cf50cab
syugesh <syugesh@griddynamics.com>
2026-07-17 01:33:50 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "REPLACE_WITH_UNIQUE_S3_BUCKET"
+    bucket       = "petclinic-terraform-state-yugesh"
     key          = "spring-petclinic/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
